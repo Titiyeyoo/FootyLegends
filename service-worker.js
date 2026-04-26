f1ed11d7fa5e24c3e8e8cb114e4fb4fa301cfc11
Titiyeyoo <paweltokarzewski90@gmail.com>
2026-04-26 08:25:45 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 // clients that a new version is ready.
 // ════════════════════════════════════════════════════════════════════════════
 
-const CACHE_VERSION = 'v5';
+const CACHE_VERSION = 'v7';
 const CORE_CACHE = 'footy-core-' + CACHE_VERSION;
 const FONT_CACHE = 'footy-fonts-' + CACHE_VERSION;
 
