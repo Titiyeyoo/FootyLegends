d093525c4f17be2fa2d778b0179e1b00bc321050
Titiyeyoo <paweltokarzewski90@gmail.com>
2026-06-15 10:05:34 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 // clients that a new version is ready.
 // ════════════════════════════════════════════════════════════════════════════
 
-const CACHE_VERSION = 'v223';
+const CACHE_VERSION = 'v224';
 const CORE_CACHE = 'footy-core-' + CACHE_VERSION;
 const FONT_CACHE = 'footy-fonts-' + CACHE_VERSION;
 
